a64737efdf47ebb5b81db473b15b712ad48bdb4e
George Li <gli.eggfault@gmail.com>
2016-01-09 02:49:41 -0800

    Add initial project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SouthGate::Application.config.secret_key_base = 'b0e7f8634d97172241e984bfa531142023c5568df2fc4ebc79e0f75b4749fceef285b078f179db148a0670c0fe0feef395f36b36b71b074f837e860a78c7f747'
